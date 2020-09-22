1330e4139242f459a8621941bed3b32134db14f2
Otavio Moura Alves <otaviomalvess@gmail.com>
2020-09-22 19:05:52 -0300

    Loss transition, renaming variables, collision improvements

--- Gravity/Assets/Scripts/Player.cs
+++ Gravity/Assets/Scripts/Player.cs
@@ -7,33 +7,33 @@ public class Player : MonoBehaviour
 	#region Serialized
 		[SerializeField] LayerMask 	whatIsGround;	// A mask determining what is ground to the character
 		[SerializeField] Transform 	checkGround;	// A position marking where to check if the player is grounded.
-		[SerializeField] Transform 	checkCeiling;	// A position marking where to check for ceilings
 		[SerializeField] Transform 	checkPoint;
 	#endregion
 
 	#region Consts
 		const float SpeedRun 		= 10f;
-		const float JumpForce 		= 16f;
+		const float JumpForce 		= 1850f;
 		const float GForce 			= 13f;
 		const float VelBulletTime 	= 2f;
 	#endregion
 
 	#region Vars
-		bool 		isGrounded	= false;	// Whether or not the player is grounded
-		bool 		isDead 		= false;	
-		bool 		jump 		= false;
-		bool 		gChange 	= false;
-		bool 		moveVer 	= false;
-		float 		moveX 		= 0f;
-		Vector2 	jumpDir 	= Vector2.up * JumpForce;
-		Vector2 	dirGravity 	= Vector2.down;
-		Vector3 	moveVel		= Vector3.zero;
-		Rigidbody2D rb;
+		bool 			isGrounded	= false;
+		bool 			isDead 		= false;
+		bool 			isJumping 	= false;
+		bool 			isGChange 	= false;
+		bool 			isMoveVer 	= false; 	// Whether the player movement is in the X or Y axis 
+		float 			moveX 		= 0f;
+		Vector2 		jumpDir 	= Vector2.up * JumpForce;
+		Vector2 		dirGravity 	= Vector2.down;
+		Rigidbody2D 	rb;
+		SpriteRenderer 	sr;
 	#endregion
 
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
 	}
 
 	void Start()
@@ -43,10 +43,10 @@ public class Player : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetButton("Change Gravity")) gChange = true; // TODO: add bullet time
+		if (Input.GetButton("Change Gravity")) isGChange = true; // TODO: add bullet time
 
 		// Change gravity
-		if (gChange) {
+		if (isGChange) {
 			if (Input.GetKey(KeyCode.W)) ChangeGravity(Vector2.up);
 			if (Input.GetKey(KeyCode.S)) ChangeGravity(Vector2.down);
 			if (Input.GetKey(KeyCode.A)) ChangeGravity(Vector2.left);
@@ -56,12 +56,12 @@ public class Player : MonoBehaviour
 		}
 		
 		// Walk
-		if (!moveVer) 	moveX = Input.GetAxisRaw("Horizontal") 	* SpeedRun;
+		if (!isMoveVer) moveX = Input.GetAxisRaw("Horizontal") 	* SpeedRun;
 		else 			moveX = Input.GetAxisRaw("Vertical") 	* SpeedRun;
 
 		// Jump
-		if (Input.GetButtonDown("Jump")) 	jump = true;
-		if (Input.GetButtonUp("Jump")) 		jump = false;
+		if (Input.GetButtonDown("Jump")) 	isJumping = true;
+		if (Input.GetButtonUp("Jump")) 		isJumping = false;
 	}
 
 	void FixedUpdate()
@@ -71,31 +71,29 @@ public class Player : MonoBehaviour
 		// Gravity
 		rb.velocity += GForce * rb.mass * dirGravity * Time.fixedDeltaTime;
 
+		Collision();
 		Move();
 		Jump();
-		gChange = false;
+		isGChange = false;
 	}
 
 	void Move()
 	{
 		// Walk
-		if (dirGravity.y != 0) 	rb.velocity = new Vector2(moveX			, rb.velocity.y);
-		else 					rb.velocity = new Vector2(rb.velocity.x	, moveX);
+		if (!isMoveVer) rb.velocity = new Vector2(moveX			, rb.velocity.y);
+		else 			rb.velocity = new Vector2(rb.velocity.x	, moveX);
 
 		// TODO: apply acceleration over small amout of time
 	}
 
 	void Jump()
 	{
-		if (isGrounded && jump)
-		{
-			rb.velocity += jumpDir;
-			return;
-		}
-		
-		// TODO: improve
+		if (!isGrounded && IsFalling()) { isJumping = false; return; }
+
+		// TODO: jump is stronger at the X axis
 		// The longer holds the button, higher the jump
-		if (rb.velocity.y > 0 && jump) rb.velocity += jumpDir * Time.fixedDeltaTime;
+		if 		(isGrounded && isJumping) 	rb.AddForce(jumpDir);
+		else if (!isJumping) 				rb.AddForce(-jumpDir / 4);
 	}
 
 	void ChangeGravity(Vector2 dir)
@@ -104,44 +102,59 @@ public class Player : MonoBehaviour
 		float rotAngle;
 		if (dir.x != 0f) 	rotAngle = dir.x * 90f;
 		else 				rotAngle = dir.y > 0f ? 180f : 0f;
-		transform.rotation = Quaternion.Euler(0f, 0f, rotAngle);
+
+		transform.rotation = Quaternion.Euler(0f, 0f, rotAngle); // TODO: smooth rotation
 
 		dirGravity 	= dir; 					// Gravity direction
-		jumpDir 	= -dir * JumpForce; 	// Jump direction		
-		moveVer 	= dir.y == 0f; 			// Vertical move
+		jumpDir 	= -dir * JumpForce; 	// Jump direction
+		isMoveVer 	= dir.y == 0f; 			// Vertical move
 	}
 
-	void Spawn()
+	bool IsFalling()
 	{
-		// TODO: transition
-		rb.position = checkPoint.position;
-		ChangeGravity(Vector2.down);
-		isGrounded 	= true;
+		if 		(dirGravity == Vector2.down) 	return rb.velocity.y < 0;
+		else if (dirGravity == Vector2.up) 		return rb.velocity.y > 0;
+		else if (dirGravity == Vector2.right) 	return rb.velocity.x > 0;
+		return rb.velocity.x < 0;
 	}
+
+	#region (Re)Spawn
+		void Respawn()
+		{
+			isDead 		= true;
+			sr.enabled 	= false;
+			
+			StartCoroutine(Transition());
+		}
+
+		IEnumerator Transition()
+		{
+			yield return new WaitForSeconds(2);
+			Spawn();
+		}
+
+		void Spawn()
+		{
+			rb.position = checkPoint.position;
+			ChangeGravity(Vector2.down);
+			sr.enabled 	= true;
+			isDead 		= false;
+		}
+	#endregion
 
 	#region OnCollision
+
+		void Collision()
+		{
+			Collider2D col 	= Physics2D.OverlapBox(checkGround.transform.position, new Vector2(.7f, .2f), 0f, whatIsGround);
+			isGrounded 		= col != null;
+		}
+
 		void OnCollisionEnter2D(Collision2D col)
 		{
 			switch(col.gameObject.layer)
 			{
-				case 8: 	isGrounded = true; 	return;
-				case 10: 	Spawn(); 			return;
-			}
-		}
-
-		void onCollisionStay2D(Collision2D col)
-		{
-			switch(col.gameObject.layer)
-			{
-				case 8: isGrounded = true; return;
-			}
-		}
-
-		void OnCollisionExit2D(Collision2D col)
-		{
-			switch(col.gameObject.layer)
-			{
-				case 8: isGrounded = false; return;
+				case 10: Respawn(); return;
 			}
 		}
 	#endregion
